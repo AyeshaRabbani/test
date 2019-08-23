3455ec35335ac70beba8a77bc7647b4fe86b8e0f
AyeshaRabbani <ayesharabbani4@gmail.com>
2019-08-24 00:38:27 +0530

    bsearch c prog

---
+++ bsearch.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+int binary(int a[100],int low,int high,int key)
+{
+  if(low<=high)
+  {
+    int mid;
+    mid=(low+high)/2;
+    if(a[mid]==key)
+      return mid;
+    else if(key>a[mid])
+      return binary(a,mid+1,high,key);
+    else
+      return binary(a,low,mid-1,key);
+  }
+  else
+    return -1;
+}
+
+
+int main(void)
+{
+  int n,a[100],k,pos,i;
+  printf("\n enter the number of element ");
+  scanf("%d",&n);
+  printf("\n enter elements ");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  printf("\n enter key");
+  scanf("%d",&k);
+  pos=binary(a,0,n-1,k);
+  if(pos==-1)
+   printf("\n not found ");
+  else
+    printf("\n found at pos %d",pos);
+  return 0;
+}
